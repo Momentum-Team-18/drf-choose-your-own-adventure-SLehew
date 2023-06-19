2682590f32db551e90970ce698365dadef6f723a
Steve Lehew <120685948+SLehew@users.noreply.github.com>
2023-06-19 12:16:41 -0400

    postgres setup and ready for Render

--- library/settings.py
+++ library/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 if env("RENDER"):
     ALLOWED_HOSTS.append(env("RENDER_EXTERNAL_HOSTNAME"))
