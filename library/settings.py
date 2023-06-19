031c21ff4b0c3e3a40902bb12162ccd60c41c23c
Steve Lehew <120685948+SLehew@users.noreply.github.com>
2023-06-19 11:46:09 -0400

    postgres setup and ready for Render

--- library/settings.py
+++ library/settings.py
@@ -137,10 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static',)]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
